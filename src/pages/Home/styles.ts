a94ee52ad77edb04feac0ee2bab6f44648c6bffd
matheus.s.pinheiro@live.com <matheus.s.pinheiro@live.com>
2023-08-07 11:28:38 -0300

    [fix:]style-fix

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,8 +1,9 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export const HomeContainer = styled.main`
   width: 100%;
   margin: 4rem auto 0;
   max-width: 1120px;
   padding: 0 1.5rem;
-`
+  color: black;
+`;
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,8 +1,6 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Rubik:wght@400;700&display=swap');
-  
   * {
     margin: 0;
     padding: 0;
